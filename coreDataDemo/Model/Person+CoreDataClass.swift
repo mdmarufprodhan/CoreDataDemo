670bd8b202a82972f66d6eb560d6778e2179fa90
mdmarufprodhan <marufprodhan463@gmail.com>
2021-02-16 02:17:11 +0600

    create attributes

---
+++ coreDataDemo/Model/Person+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Person+CoreDataClass.swift
+//  coreDataDemo
+//
+//  Created by Md Maruf Prodhan on 16/2/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
